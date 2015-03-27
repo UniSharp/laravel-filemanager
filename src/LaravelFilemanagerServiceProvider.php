0345b5069b95dc614bd5945806cec7cc9f2f5dcd
Trevor Sawler <trevor.sawler@gmail.com>
2015-03-27 12:51:27 -0300

    In progress

---
+++ src/LaravelFilemanagerServiceProvider.php
@@ -0,0 +1,56 @@
+<?php namespace Tsawler\Laravelfilemanager;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+
+
+class LaravelFilemanagerServiceProvider extends ServiceProvider {
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__ . '/routes.php';
+
+        //$this->loadTranslationsFrom(__DIR__.'/lang', 'vcms5');
+
+        $this->loadViewsFrom(__DIR__.'/views', 'vcms5');
+
+//        $this->publishes([
+//            __DIR__ . '/config/vcms5.php' => config_path('vcms5.php', 'config'),
+//        ], 'v_config');
+//
+//        $this->publishes([
+//            __DIR__.'/../public' => public_path('vendor/vcms5'),
+//        ], 'v_public');
+//
+//        $this->publishes([
+//            __DIR__.'/migrations' => base_path('/database/migrations'),
+//        ], 'v_migrations');
+//
+//        $this->publishes([
+//            __DIR__.'/views' => base_path('resources/views/vendor/vcms5'),
+//        ], 'v_views');
+//
+//        $this->publishes([
+//            __DIR__.'/seeds' => base_path('database/seeds'),
+//        ], 'seeds');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app['laravel-filemanager'] = $this->app->share(function ()
+        {
+            return true;
+        });
+    }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+
+Route::get('/laravel-filemanager', function(){
+   return "Hello";
+});
