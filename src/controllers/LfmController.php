722975f39c8763de6d0a29000f49b29046a3d8c0
Trevor Sawler <trevor.sawler@gmail.com>
2015-03-28 09:54:46 -0300

    stub

---
+++ src/controllers/LfmController.php
@@ -0,0 +1,15 @@
+<?php namespace Tsawler\Laravelfilemanager\controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\View;
+
+
+/**
+ * Class LfmController
+ * @package Tsawler\Laravelfilemanager\controllers
+ */
+class LfmController extends Controller {
+
+
+}
