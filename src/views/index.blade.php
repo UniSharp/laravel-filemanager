5cde4ba62f3bdcf0a36fded276d267b56a5b4615
Trevor Sawler <trevor.sawler@gmail.com>
2015-03-31 17:49:47 -0300

    add default thumb

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -317,7 +317,6 @@
         function getUrlParam( paramName ) {
             var reParam = new RegExp( '(?:[\?&]|&)' + paramName + '=([^&]+)', 'i' ) ;
             var match = window.location.search.match(reParam) ;
-
             return ( match && match.length > 1 ) ? match[ 1 ] : null ;
         }
         var funcNum = getUrlParam( 'CKEditorFuncNum' );
