153fe5b726f85c5f1ff27c0908cc3d9123695221
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-02 00:05:15 -0300

    Scaling

--- src/controllers/ScaleController.php
+++ src/controllers/ScaleController.php
@@ -17,17 +17,36 @@ class ScaleController extends Controller {
      */
     public function getScale()
     {
+        $ratio = 1.0;
         $image = Input::get('img');
         $dir = Input::get('dir');
 
-        $width = Image::make(base_path() . "/" . Config::get('lfm.images_dir') . $dir . "/" .  $image)->width();
-        $height = Image::make(base_path() . "/" . Config::get('lfm.images_dir') . $dir . "/" .  $image)->height();
+        $original_width = Image::make(base_path() . "/" . Config::get('lfm.images_dir') . $dir . "/" . $image)->width();
+        $original_height = Image::make(base_path() . "/" . Config::get('lfm.images_dir') . $dir . "/" . $image)->height();
+
+        $scaled = false;
+
+        if ($original_width > 600)
+        {
+            $ratio = 600 / $original_width;
+            $width = $original_width * $ratio;
+            $height = $original_height * $ratio;
+            $scaled = true;
+        } else
+        {
+            $height = $original_height;
+            $width = $original_width;
+        }
 
         return View::make('laravel-filemanager::scale')
             ->with('img', Config::get('lfm.images_url') . $dir . "/" . $image)
             ->with('dir', $dir)
             ->with('image', $image)
-            ->with('height', $height)
-            ->with('width', $width);
+            ->with('height', number_format($height, 0))
+            ->with('width', $width)
+            ->with('original_height', $original_height)
+            ->with('original_width', $original_width)
+            ->with('scaled', $scaled)
+            ->with('ratio', $ratio);
     }
 }
