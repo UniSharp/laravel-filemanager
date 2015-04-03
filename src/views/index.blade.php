d80e1e2d9c59fe5e14877cb2f1ed0fdbbb9f47a4
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-03 10:13:03 -0300

    Move validation into controller to avoid namespace problems

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -120,6 +120,7 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <div class="alert alert-danger" role="alert">
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                             <ul>
                                                 @foreach($errors->all() as $error)
                                                     <li>{{ $error }}</li>
