3f4a134d62765fee8de56665b7a9360ec1029a72
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-04 12:39:17 -0300

    Make file view modal larger

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -116,7 +116,7 @@
 </div>
 
 <div class="modal fade" id="fileViewModal" tabindex="-1" role="dialog" aria-labelledby="fileLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
