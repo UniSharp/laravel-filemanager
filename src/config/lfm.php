2cd8f7db547cfd5fed72692901384eedcd296423
Trevor Sawler <trevor.sawler@gmail.com>
2015-04-06 14:14:53 -0300

    Remove unused method; formatting

--- src/config/lfm.php
+++ src/config/lfm.php
@@ -9,8 +9,6 @@ return [
     'files_dir'          => 'public/vendor/laravel-filemanager/files/',
     'files_url'          => '/vendor/laravel-filemanager/files/',
 
-    'params'             => 'type=Images&CKEditor=editor&CKEditorFuncNum=1&langCode=en',
-
     'type_array'         => [
         "pdf"  => "Adobe Acrobat",
         "docx" => "Microsoft Word",
@@ -27,17 +25,17 @@ return [
     ],
 
     'icon_array'         => [
-                                "pdf"  => "fa-file-pdf-o",
-                                "docx" => "fa-file-word-o",
-                                "docx" => "fa-file-word-o",
-                                "xls"  => "fa-file-excel-o",
-                                "xls"  => "fa-file-excel-o",
-                                "zip"  => 'fa-file-archive-o',
-                                "gif"  => 'fa-file-image-o',
-                                "jpg"  => 'fa-file-image-o',
-                                "jpeg" => 'fa-file-image-o',
-                                "png"  => 'fa-file-image-o',
-                                "ppt"  => 'fa-file-powerpoint-o',
-                                "pptx" => 'fa-file-powerpoint-o',
-                            ],
+        "pdf"  => "fa-file-pdf-o",
+        "docx" => "fa-file-word-o",
+        "docx" => "fa-file-word-o",
+        "xls"  => "fa-file-excel-o",
+        "xls"  => "fa-file-excel-o",
+        "zip"  => 'fa-file-archive-o',
+        "gif"  => 'fa-file-image-o',
+        "jpg"  => 'fa-file-image-o',
+        "jpeg" => 'fa-file-image-o',
+        "png"  => 'fa-file-image-o',
+        "ppt"  => 'fa-file-powerpoint-o',
+        "pptx" => 'fa-file-powerpoint-o',
+    ],
 ];
