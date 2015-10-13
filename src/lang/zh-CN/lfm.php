6aab3bd7b4cbf84eca3bce8a6e0f8059e9d22c56
StreamHuang <g0110280@gmail.com>
2015-10-13 17:05:34 +0800

    add zh-CN locale

---
+++ src/lang/zh-CN/lfm.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'nav-upload'        => '上传档案',
+    'nav-thumbnails'    => '缩略图显示',
+    'nav-list'          => '列表显示',
+
+    'menu-new'          => '添加文件夹',
+    'menu-rename'       => '重命名',
+    'menu-delete'       => '删除',
+    'menu-view'         => '预览',
+    'menu-download'     => '下载',
+    'menu-resize'       => '缩放',
+    'menu-crop'         => '裁剪',
+
+    'title-page'        => '档案管理',
+    'title-panel'       => '档案管理',
+    'title-upload'      => '上传档案',
+    'title-view'        => '预览档案',
+    'title-root'        => '我的档案',
+    'title-item'        => '项目名称',
+    'title-size'        => '档案大小',
+    'title-type'        => '档案类型',
+    'title-modified'    => '上次修改',
+    'title-action'      => '操作',
+
+    'type-folder'       => '文件夹',
+
+    'message-empty'     => '空的文件夹',
+    'message-choose'    => '选择档案',
+    'message-delete'    => '确定要删除此项目吗？',
+    'message-name'      => '文件夹名称:',
+    'message-rename'    => '重命名为:',
+
+    'btn-upload'        => '上传',
+    'btn-close'         => '关闭',
+    'btn-uploading'     => '上传中...',
+    'btn-crop'          => '裁剪',
+    'btn-cancel'        => '取消',
+    'btn-resize'        => '缩放',
+
+    'resize-ratio'      => '比例:',
+    'resize-scaled'     => '是否已缩放:',
+    'resize-true'       => '是',
+    'resize-old-height' => '原始高度:',
+    'resize-old-width'  => '原始宽度:',
+    'resize-new-height' => '目前高度:',
+    'resize-new-width'  => '目前宽度:',
+
+    'locale-bootbox'    => 'zh_CN',
+
+];
