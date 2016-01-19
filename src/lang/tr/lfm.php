9a75530b367db9a65a25a2cd581758ed7a13ce8b
MMT <1@44mb.net>
2016-01-19 17:37:16 +0200

    Add Turkish translation

---
+++ src/lang/tr/lfm.php
@@ -0,0 +1,60 @@
+<?php
+
+return [
+    'nav-upload'        => 'Yükle',
+    'nav-thumbnails'    => 'Küçük Resim',
+    'nav-list'          => 'Liste',
+
+    'menu-new'          => 'Yeni Klasör',
+    'menu-rename'       => 'Ad değiştir',
+    'menu-delete'       => 'Sil',
+    'menu-view'         => 'Görüntüle',
+    'menu-download'     => 'İndir',
+    'menu-resize'       => 'Boyutlandır',
+    'menu-crop'         => 'Kırp',
+
+    'title-page'        => 'Dosya Kütüphanesi',
+    'title-panel'       => 'Laravel Dosya Kütüphanesi',
+    'title-upload'      => 'Dosya Yükle',
+    'title-view'        => 'Dosya Gör',
+    'title-root'        => 'Dosyalarım',
+    'title-shares'      => 'Paylaşılan Dosyalar',
+    'title-item'        => 'Dosya',
+    'title-size'        => 'Boyut',
+    'title-type'        => 'Tür',
+    'title-modified'    => 'Güncelleme',
+    'title-action'      => 'Komutlar',
+
+    'type-folder'       => 'Klasör',
+
+    'message-empty'     => 'Klasör boş.',
+    'message-choose'    => 'Dosya seç',
+    'message-delete'    => 'Bu dosyayı silmek istediğinizden emin misiniz?',
+    'message-name'      => 'Klasör adı:',
+    'message-rename'    => 'Yeni ad:',
+
+    'error-rename'      => 'Dosya adı kullanımda!',
+    'error-file-empty'  => 'Bir dosya seçmelisiniz!',
+    'error-file-exist'  => 'Bu adda bir dosya zaten var!',
+    'error-delete'      => 'Klasör boş olmadığından, klasörü silemezsiniz!',
+    'error-folder-name' => 'Klasör adı yazılmalıdır!',
+    'error-folder-exist'=> 'Bu adda bir klasör zaten var!',
+
+    'btn-upload'        => 'Yükle',
+    'btn-close'         => 'Kapat',
+    'btn-uploading'     => 'Yükleniyor...',
+    'btn-crop'          => 'Kırp',
+    'btn-cancel'        => 'İptal',
+    'btn-resize'        => 'Boyutlandır',
+
+    'resize-ratio'      => 'Oran:',
+    'resize-scaled'     => 'Boyutlandırıldı mı:',
+    'resize-true'       => 'Evet',
+    'resize-old-height' => 'Orijinal Yükseklik:',
+    'resize-old-width'  => 'Orijinal Genişlik:',
+    'resize-new-height' => 'Yükseklik:',
+    'resize-new-width'  => 'Genişlik:',
+
+    'locale-bootbox'    => 'tr',
+
+];
