b23fd1490d391719205da27e81b460afd28acd70
Jan-Joost Heurter <janjoost@u-lab.nl>
2016-07-28 16:36:28 +0200

    Fixes on the saving of uploaded files

--- src/controllers/UploadController.php
+++ src/controllers/UploadController.php
@@ -11,166 +11,190 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * Class UploadController
+ *
  * @package Jayked\Laravelfilemanager\controllers
  */
-class UploadController extends LfmController {
+class UploadController extends LfmController
+{
 
-    private $default_file_types = ['application/pdf'];
-    private $default_image_types = ['image/jpeg', 'image/png', 'image/gif'];
+	private $default_file_types = [ 'application/pdf' ];
+	private $default_image_types = [ 'image/jpeg', 'image/png', 'image/gif' ];
 
-    /**
-     * Upload an image/file and (for images) create thumbnail
-     *
-     * @return string
-     */
-    public function upload()
-    {
-        dd('test');
-        try {
-            $res = $this->uploadValidator();
-            if (true !== $res) {
-                return Lang::get('laravel-filemanager::lfm.error-invalid');
-            }
-        } catch (\Exception $e) {
-            return $e->getMessage();
-        }
+	/**
+	 * Upload an image/file and (for images) create thumbnail
+	 *
+	 * @return string
+	 */
+	public function upload()
+	{
+		try
+		{
+			$res = $this->uploadValidator();
+			if( true !== $res )
+			{
+				return Lang::get( 'laravel-filemanager::lfm.error-invalid' );
+			}
+		}
+		catch( \Exception $e )
+		{
+			return $e->getMessage();
+		}
 
-        $file = Input::file('upload');
+		$file = Input::file( 'upload' );
 
-        $new_filename = $this->getNewName($file);
+		$new_filename = $this->getNewName( $file );
 
-        $dest_path = parent::getPath('directory');
+		$dest_path = parent::getPath( 'directory' );
 
-        if (File::exists($dest_path . $new_filename)) {
-            return Lang::get('laravel-filemanager::lfm.error-file-exist');
-        }
+		if( File::exists( $dest_path . $new_filename ) )
+		{
+			return Lang::get( 'laravel-filemanager::lfm.error-file-exist' );
+		}
 
-        $file->move($dest_path, $new_filename);
+		$file->move( $dest_path, $new_filename );
 
-        if ('Images' === $this->file_type) {
-            $this->makeThumb($dest_path, $new_filename);
-        }
+		if( 'Images' === $this->file_type )
+		{
+			$this->makeThumb( $dest_path, $new_filename );
+		}
 
-        // upload via ckeditor 'Upload' tab
-        if (!Input::has('show_list')) {
-            return $this->useFile($new_filename);
-        }
+		// upload via ckeditor 'Upload' tab
+		if( !Input::has( 'show_list' ) )
+		{
+			return $this->useFile( $new_filename );
+		}
 
-        return 'OK';
-    }
+		return 'OK';
+	}
 
-    /**
-     * Validation of the uploaded file
-     *
-     * @return bool
-     * @throws \Exception
-     */
-    private function uploadValidator()
-    {
-        // when uploading a file with the POST named "upload"
+	/**
+	 * Validation of the uploaded file
+	 *
+	 * @return bool
+	 * @throws \Exception
+	 */
+	private function uploadValidator()
+	{
+		// when uploading a file with the POST named "upload"
 
-        $expected_file_type = $this->file_type;
-        $is_valid = false;
+		$expected_file_type = $this->file_type;
+		$is_valid           = false;
 
-        $file = Input::file('upload');
-        if (empty($file)) {
-            throw new \Exception(Lang::get('laravel-filemanager::lfm.error-file-empty'));
-        }
-        if (!$file instanceof UploadedFile) {
-            throw new \Exception(Lang::get('laravel-filemanager::lfm.error-instance'));
-        }
+		$file = Input::file( 'upload' );
+		if( empty( $file ) )
+		{
+			throw new \Exception( Lang::get( 'laravel-filemanager::lfm.error-file-empty' ) );
+		}
+		if( !$file instanceof UploadedFile )
+		{
+			throw new \Exception( Lang::get( 'laravel-filemanager::lfm.error-instance' ) );
+		}
 
-        $mimetype = $file->getMimeType();
+		$mimetype = $file->getMimeType();
 
-        if ($expected_file_type === 'Files') {
-            $config_name = 'lfm.valid_file_mimetypes';
-            $valid_mimetypes = Config::get($config_name, $this->default_file_types);
-        } else {
-            $config_name = 'lfm.valid_image_mimetypes';
-            $valid_mimetypes = Config::get($config_name, $this->default_image_types);
-        }
+		if( $expected_file_type === 'Files' )
+		{
+			$config_name     = 'lfm.valid_file_mimetypes';
+			$valid_mimetypes = Config::get( $config_name, $this->default_file_types );
+		}
+		else
+		{
+			$config_name     = 'lfm.valid_image_mimetypes';
+			$valid_mimetypes = Config::get( $config_name, $this->default_image_types );
+		}
 
-        if (!is_array($valid_mimetypes)) {
-            throw new \Exception('Config : ' . $config_name . ' is not set correctly');
-        }
+		if( !is_array( $valid_mimetypes ) )
+		{
+			throw new \Exception( 'Config : ' . $config_name . ' is not set correctly' );
+		}
 
-        if (in_array($mimetype, $valid_mimetypes)) {
-            $is_valid = true;
-        }
+		if( in_array( $mimetype, $valid_mimetypes ) )
+		{
+			$is_valid = true;
+		}
 
-        if (false === $is_valid) {
-            throw new \Exception(Lang::get('laravel-filemanager::lfm.error-mime') . $mimetype);
-        }
-        return $is_valid;
-    }
+		if( false === $is_valid )
+		{
+			throw new \Exception( Lang::get( 'laravel-filemanager::lfm.error-mime' ) . $mimetype );
+		}
 
-    /**
-     * Retrieve a name for the file
-     *
-     * @param $file
-     * @return mixed|string
-     */
-    private function getNewName($file)
-    {
-        $dest_path = parent::getPath('directory');
+		return $is_valid;
+	}
 
-        $new_filename = $this->existingFile( $dest_path, $file );
+	/**
+	 * Retrieve a name for the file
+	 *
+	 * @param $file
+	 * @return mixed|string
+	 */
+	private function getNewName( $file )
+	{
+		$dest_path = parent::getPath( 'directory' );
 
-        return $new_filename;
-    }
+		$new_filename = $this->existingFile( $dest_path, $file );
 
-    /**
-     * Check if the file already exists with uniqid()
-     *
-     * @param $path
-     * @param $file
-     * @return string
-     */
-    private function existingFile( $path, $file )
-    {
-        $name = $file->getClientOriginalName() . '_' . uniqid();
-        $name .= '.' . $file->getClientOriginalExtension();
+		return $new_filename;
+	}
 
-        // If a file with the name allready exists
-        if( File::exists( $path . $name ) )
-        {
-            return self::existingFile( $path, $file );
-        }
+	/**
+	 * Check if the file already exists with uniqid()
+	 *
+	 * @param $path
+	 * @param $file
+	 * @param null $extension
+	 * @return string
+	 */
+	private function existingFile( $path, $file, $extension = null )
+	{
+		$name = pathinfo( $file->getClientOriginalName(), PATHINFO_FILENAME );
 
-        return $name;
-    }
+		if( $extension !== null )
+		{
+			$name .= '_' . $extension;
+		}
+		$name .= '.' . $file->getClientOriginalExtension();
 
-    /**
-     * Generate a small thumb for the file
-     *
-     * @param $dest_path
-     * @param $new_filename
-     */
-    private function makeThumb($dest_path, $new_filename)
-    {
-        $thumb_folder_name = Config::get('lfm.thumb_folder_name');
+		// If a file with the name already exists
+		if( File::exists( $path . $name ) )
+		{
+			return self::existingFile( $path, $file, uniqid() );
+		}
 
-        if (!File::exists($dest_path . $thumb_folder_name)) {
-            File::makeDirectory($dest_path . $thumb_folder_name);
-        }
+		return $name;
+	}
 
-        $thumb_img = Image::make($dest_path . $new_filename);
-        $thumb_img->fit(200, 200)
-            ->save($dest_path . $thumb_folder_name . '/' . $new_filename);
-        unset($thumb_img);
-    }
+	/**
+	 * Generate a small thumb for the file
+	 *
+	 * @param $dest_path
+	 * @param $new_filename
+	 */
+	private function makeThumb( $dest_path, $new_filename )
+	{
+		$thumb_folder_name = Config::get( 'lfm.thumb_folder_name' );
 
-    /**
-     * Use the file if it has been uploaded via CKEditor or TinyMCE
-     *
-     * @param $new_filename
-     * @return string
-     */
-    private function useFile($new_filename)
-    {
-        $file = parent::getUrl() . $new_filename;
+		if( !File::exists( $dest_path . $thumb_folder_name ) )
+		{
+			File::makeDirectory( $dest_path . $thumb_folder_name );
+		}
 
-        return "<script type='text/javascript'>
+		$thumb_img = Image::make( $dest_path . $new_filename );
+		$thumb_img->fit( 200, 200 )
+			->save( $dest_path . $thumb_folder_name . '/' . $new_filename );
+		unset( $thumb_img );
+	}
+
+	/**
+	 * Use the file if it has been uploaded via CKEditor or TinyMCE
+	 *
+	 * @param $new_filename
+	 * @return string
+	 */
+	private function useFile( $new_filename )
+	{
+		$file = parent::getUrl() . $new_filename;
+
+		return "<script type='text/javascript'>
 
         function getUrlParam(paramName) {
             var reParam = new RegExp('(?:[\?&]|&)' + paramName + '=([^&]+)', 'i');
@@ -187,6 +211,6 @@ class UploadController extends LfmController {
         if (op) window.close();
         if (o !== false) o.CKEDITOR.tools.callFunction(funcNum, '$file');
         </script>";
-    }
+	}
 
 }
