7238108665f9a9bf36e944b64257f802bccb9e7b
Jan-Joost Heurter <janjoost@u-lab.nl>
2016-09-23 09:34:29 +0200

    Fix actions in list views

--- src/views/images-list.blade.php
+++ src/views/images-list.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
 
   @if((sizeof($file_info) > 0) || (sizeof($directories) > 0))
-  <table class="table table-condensed table-striped">
+    <table class="table table-condensed table-striped">
     <thead>
       <th>{{ Lang::get('laravel-filemanager::lfm.title-item') }}</th>
       <th>{{ Lang::get('laravel-filemanager::lfm.title-size') }}</th>
@@ -9,20 +9,20 @@
       <th>{{ Lang::get('laravel-filemanager::lfm.title-modified') }}</th>
       <th>
         @if($options['remove'] || $options['crop'] || $options['resize'])
-        {{ Lang::get('laravel-filemanager::lfm.title-action') }}
+          {{ Lang::get('laravel-filemanager::lfm.title-action') }}
         @endif
       </th>
     </thead>
     <tbody>
       @foreach($directories as $key => $dir_name)
-      <tr>
+        <tr>
         <td>
           <i class="fa fa-folder-o"></i>
-          <a class="folder-item pointer" data-id="{{ $dir_name['long'] }}">
+          <a class="folder-item pointer" data-id="{{ $dir_name['long'] }}" title="{{ $dir_name['base'] }}">
             {{ $dir_name['base'] }}
           </a>
           @if($options['rename'])
-            <a href="javascript:rename('{{ $dir_name['base'] }}')"><i class="fa fa-edit fa-fw"></i></a>
+            <a href="javascript:rename('{{ $dir_name['base'] }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-rename') }}"><i class="fa fa-edit fa-fw"></i></a>
           @endif
         </td>
         <td></td>
@@ -30,23 +30,23 @@
         <td></td>
         <td>
           @if($options['remove'])
-            <a href="javascript:trash('{{ $dir_name['base'] }}')"><i class="fa fa-trash fa-fw"></i></a>
+            <a href="javascript:trash('{{ $dir_name['base'] }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-delete') }}"><i class="fa fa-trash fa-fw"></i></a>
           @endif
         </td>
       </tr>
       @endforeach
 
       @foreach($file_info as $file)
-      <tr>
+        <tr>
         <td>
           <i class="fa fa-image"></i>
           <?php $file_name = $file['name'];?>
-          <a href="javascript:useFile('{{ $file_name }}')">
+          <a href="javascript:useFile('{{ $file_name }}')" title="{{ $file_name }}">
             {{ $file_name }}
           </a>
           @if($options['rename'])
-          &nbsp;&nbsp;
-          <a href="javascript:rename('{{ $file_name }}')">
+            &nbsp;&nbsp;
+            <a href="javascript:rename('{{ $file_name }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-rename') }}">
             <i class="fa fa-edit"></i>
           </a>
           @endif
@@ -61,18 +61,20 @@
           {{ date("Y-m-d h:m", $file['created']) }}
         </td>
         <td>
+          <a href="javascript:fileView('{{ $file_name }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-view') }}"><i class="fa fa-image fa-fw"></i></a>
+          <a href="javascript:download('{{ $file_name }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-download') }}"><i class="fa fa-download fa-fw"></i></a>
           @if($options['remove'])
-          <a href="javascript:trash('{{ $file_name }}')">
+            <a href="javascript:trash('{{ $file_name }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-delete') }}">
             <i class="fa fa-trash fa-fw"></i>
           </a>
           @endif
           @if($options['crop'])
-          <a href="javascript:cropImage('{{ $file_name }}')">
+            <a href="javascript:cropImage('{{ $file_name }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-crop') }}">
             <i class="fa fa-crop fa-fw"></i>
           </a>
           @endif
           @if($options['resize'])
-          <a href="javascript:resizeImage('{{ $file_name }}')">
+            <a href="javascript:resizeImage('{{ $file_name }}')" title="{{ Lang::get('laravel-filemanager::lfm.menu-resize') }}">
             <i class="fa fa-arrows fa-fw"></i>
           </a>
           @endif
@@ -86,7 +88,7 @@
   </table>
 
   @else
-  <div class="row">
+    <div class="row">
     <div class="col-md-12">
       <p>{{ Lang::get('laravel-filemanager::lfm.message-empty') }}</p>
     </div>
