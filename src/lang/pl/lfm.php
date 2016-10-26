6762214a4d886eac75dc30891d3e13d09bb413bc
zloter <zygmunt.loter@optimalit.pl>
2016-10-26 13:11:21 +0200

    Add polish translation
    Polish translation for laravel-filemanager

---
+++ src/lang/pl/lfm.php
@@ -0,0 +1,68 @@
+<?php
+
+return [
+    'nav-back'          => 'Powrót',
+    'nav-new'           => 'Nowy Folder',
+    'nav-upload'        => 'Wgraj plik',
+    'nav-thumbnails'    => 'Miniaturki',
+    'nav-list'          => 'Lista',
+
+    'menu-rename'       => 'Zmień nazwę',
+    'menu-delete'       => 'Usuń',
+    'menu-view'         => 'Wyświetl',
+    'menu-download'     => 'Pobierz',
+    'menu-resize'       => 'Zmień rozmiar',
+    'menu-crop'         => 'Przytnij',
+
+    'title-page'        => 'Menedżer plików',
+    'title-panel'       => 'Laravel FileManager',
+    'title-upload'      => 'Wgraj plik',
+    'title-view'        => 'Podgląd',
+    'title-root'        => 'Pliki',
+    'title-shares'      => 'Udostępnione pliki',
+    'title-item'        => 'Nazwa',
+    'title-size'        => 'Rozmiar',
+    'title-type'        => 'Typ',
+    'title-modified'    => 'Utworzono',
+    'title-action'      => 'Akcje',
+
+    'type-folder'       => 'Folder',
+
+    'message-empty'     => 'Przepraszamy, ten folder jest pusty.',
+    'message-choose'    => 'Wybierz plik',
+    'message-delete'    => 'Czy na pewno chcesz usunąć ten plik?',
+    'message-name'      => 'Nazwa folderu:',
+    'message-rename'    => 'Zmień nazwę:',
+    'message-extension_not_found' => 'Niestety, nie znaleziono wymaganych rozszerzeń. Zainstaluj gd lub imagick aby manipulować grafiką',
+
+    'error-rename'      => 'Niestety, istnieje już plik o takiej nazwie!',
+    'error-file-empty'  => 'You must choose a file!',
+    'error-file-exist'  => 'Niestety, istnieje już plik o takiej nazwie!',
+    'error-file-size'   => 'Przekroczono maksymalny rozmiar wgrywanych plików! (maximum size: :max)',
+    'error-delete'      => 'Nie możesz usunąć tego folderu, ponieważ nie jest pusty!',
+    'error-folder-name' => 'Nazwa folderu nie może być pusta!',
+    'error-folder-exist'=> 'Folder o tej nazwie już istnieje!',
+    'error-folder-alnum'=> 'Dozwolone są jedynie nazwy alfanumeryczne!',
+    'error-mime'        => 'Nierozpoznawany MimeType: ',
+    'error-instance'    => 'Wgrywany obiekt powinien być instanją UploadedFile',
+    'error-invalid'     => 'Nieprawidłowe zapytanie',
+    'error-other'       => 'Napotkano następujący błąd: ',
+    'error-too-large'   => 'Przekroczono dozwolony czas operacji!',
+
+    'btn-upload'        => 'Wgraj plik',
+    'btn-uploading'     => 'Wgrywanie...',
+    'btn-close'         => 'Zamknij',
+    'btn-crop'          => 'Przytnij',
+    'btn-cancel'        => 'Anuluj',
+    'btn-resize'        => 'Zmień rozmiar',
+
+    'resize-ratio'      => 'Stosunek:',
+    'resize-scaled'     => 'Zmieniono rozmiar:',
+    'resize-true'       => 'tak',
+    'resize-old-height' => 'Orginalna wysokość:',
+    'resize-old-width'  => 'Orginalna szerokość:',
+    'resize-new-height' => 'Wysokość:',
+    'resize-new-width'  => 'Szerokość:',
+
+    'locale-bootbox'    => 'pl',
+];
