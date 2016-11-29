ac019be9798a17fcf8a0e6ffb358ae1c8354c775
StreamHuang <g0110280@gmail.com>
2016-11-30 00:51:26 +0800

    rename test to ApiTest

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,9 +1,28 @@
 <?php
 
-use Unisharp\Laravelfilemanager\Tests;
-
-class TestApi extends TestCase
+class ApiTest extends Illuminate\Foundation\Testing\TestCase
 {
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require './bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     public function testFolder()
     {
         auth()->loginUsingId(1);
