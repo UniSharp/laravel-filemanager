77f65f2b41c0bb9b709b49ac02e02998b41ed633
Stream <stream@unisharp.com>
2017-04-09 23:25:22 +0800

    refactor list-view.blade.php

--- src/views/list-view.blade.php
+++ src/views/list-view.blade.php
@@ -8,54 +8,34 @@
     <th>{{ Lang::get('laravel-filemanager::lfm.title-action') }}</th>
   </thead>
   <tbody>
-    @foreach($directories as $key => $directory)
+    @foreach($items as $item)
     <tr>
       <td>
-        <i class="fa fa-folder-o"></i>
-        <a class="folder-item clickable" data-id="{{ $directory->path }}">
-          {{ $directory->name }}
+        <i class="fa {{ $item->icon }}"></i>
+        @if(!$item->is_file)
+        <a class="folder-item clickable" data-id="{{ $item->path }}">
+        @else
+        <a href="javascript:useFile('{{ $item->name }}')" id="{{ $item->name }}" data-url="{{ $item->url }}">
+        @endif
+          {{ str_limit($item->name, $limit = 20, $end = '...') }}
         </a>
       </td>
-      <td></td>
-      <td>{{ Lang::get('laravel-filemanager::lfm.type-folder') }}</td>
-      <td></td>
-      <td></td>
-    </tr>
-    @endforeach
-
-    @foreach($files as $file)
-    <tr>
+      <td>{{ $item->size ?? '' }}</td>
+      <td>{{ $item->type }}</td>
+      <td>{{ $item->time }}</td>
       <td>
-        <i class="fa {{ $file->icon }}"></i>
-        <?php $file_name = $file->name;?>
-        <a href="javascript:useFile('{{ $file_name }}')" id="{{ $file_name }}" data-url="{{ $file->url }}">
-          {{ str_limit($file_name, $limit = 20, $end = '...') }}
-        </a>
-        &nbsp;&nbsp;
-        <a href="javascript:rename('{{ $file_name }}')">
-          <i class="fa fa-edit"></i>
-        </a>
-      </td>
-      <td>
-        {{ $file->size }}
-      </td>
-      <td>
-        {{ $file->type }}
-      </td>
-      <td>
-        {{ date("Y-m-d h:m", $file->updated) }}
-      </td>
-      <td>
-        <a href="javascript:trash('{{ $file_name }}')">
+        @if($item->is_file)
+        <a href="javascript:trash('{{ $item->name }}')">
           <i class="fa fa-trash fa-fw"></i>
         </a>
-        @if($file->thumb)
-        <a href="javascript:cropImage('{{ $file_name }}')">
+        @if($item->thumb)
+        <a href="javascript:cropImage('{{ $item->name }}')">
           <i class="fa fa-crop fa-fw"></i>
         </a>
-        <a href="javascript:resizeImage('{{ $file_name }}')">
+        <a href="javascript:resizeImage('{{ $item->name }}')">
           <i class="fa fa-arrows fa-fw"></i>
         </a>
+        @endif
         @endif
       </td>
     </tr>
@@ -94,7 +74,7 @@
                 @else
                 <a href="javascript:useFile('{{ $item->name }}')" id="{{ $item->name }}" data-url="{{ $item->url }}">
                 @endif
-                  {{ str_limit($item->name, $limit = 10, $end = '...') }}
+                  {{ str_limit($item->name, $limit = 20, $end = '...') }}
                 </a>
                 &nbsp;&nbsp;
                 {{-- <a href="javascript:rename('{{ $item->name }}')">
