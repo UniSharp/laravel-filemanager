a32d0f3a6c7617e160211f6e7a27e1e503c5dd3f
Pyae Hein <pyaehein@Pyae-Heins-MacBook-Pro.local>
2017-06-05 23:17:07 +0630

    Fixed for multiple integeration with summernote and standalone button

--- public/js/lfm.js
+++ public/js/lfm.js
@@ -8,19 +8,17 @@
       localStorage.setItem('target_input', $(this).data('input'));
       localStorage.setItem('target_preview', $(this).data('preview'));
       window.open(route_prefix + '?type=' + type, 'FileManager', 'width=900,height=600');
+      window.SetUrl = function (url, file_path) {
+          //set the value of the desired input to image url
+          var target_input = $('#' + localStorage.getItem('target_input'));
+          target_input.val(file_path).trigger('change');
+
+          //set or change the preview image src
+          var target_preview = $('#' + localStorage.getItem('target_preview'));
+          target_preview.attr('src', url).trigger('change');
+      };
       return false;
     });
   }
 
 })(jQuery);
-
-
-function SetUrl(url, file_path){
-  //set the value of the desired input to image url
-  var target_input = $('#' + localStorage.getItem('target_input'));
-  target_input.val(file_path).trigger('change');
-
-  //set or change the preview image src
-  var target_preview = $('#' + localStorage.getItem('target_preview'));
-  target_preview.attr('src', url).trigger('change');
-}
