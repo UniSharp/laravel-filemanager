c4591454f8c9c42aa3be6b8b11f5301a51f9e7a9
Stream <stream@unisharp.com>
2017-06-20 10:31:12 +0800

    refactor config format

--- src/config/lfm.php
+++ src/config/lfm.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Documentation for this config :
+|--------------------------------------------------------------------------
+| online  => http://unisharp.github.io/laravel-filemanager/config
+| offline => vendor/unisharp/laravel-filemanager/docs/config.md
+*/
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -7,19 +15,11 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    // Include to pre-defined routes from package or not. Middlewares
     'use_package_routes' => true,
 
-    // Middlewares which should be applied to all package routes.
-    // For laravel 5.1 and before, remove 'web' from the array.
     'middlewares' => ['web','auth'],
 
-    // The url to this package. Change it if necessary.
-    'prefix' => 'laravel-filemanager',
-
-    // The prefix of urls to non-public files, for exmaple if: base_directory !== 'public'
-    // Without slashes
-    'urls_prefix' => '',
+    'urls_prefix' => 'laravel-filemanager',
 
     /*
     |--------------------------------------------------------------------------
@@ -27,43 +27,50 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    // If true, private folders will be created for each signed-in user.
-    'allow_multi_user' => true,
-    // If true, share folder will be created when allow_multi_user is true.
+    'allow_multi_user'   => true,
+
     'allow_share_folder' => true,
 
-    // Flexibla way to customize client folders accessibility
-    // If you want to customize client folders, publish tag="lfm_handler"
-    // Then you can rewrite userField function in App\Handler\ConfigHander class
-    // And set 'user_field' to App\Handler\ConfigHander::class
-    // Ex: The private folder of user will be named as the user id.
-    'user_field' => Unisharp\Laravelfilemanager\Handlers\ConfigHandler::class,
-
     /*
     |--------------------------------------------------------------------------
-    | Working Directory
+    | Folder Names
     |--------------------------------------------------------------------------
     */
 
-    // Which folder to store files in filesystem disk .
-    'base_directory' => 'laravel-filemanager',
-
-    'images_folder_name' => 'photos',
-    'files_folder_name'  => 'files',
+    'user_folder_name'   => Unisharp\Laravelfilemanager\Handlers\ConfigHandler::class,
 
     'shared_folder_name' => 'shares',
+
     'thumb_folder_name'  => 'thumbs',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Startup Views
-    |--------------------------------------------------------------------------
-    */
-
-    // The default display type for items.
-    // Supported: "grid", "list"
-    'images_startup_view' => 'grid',
-    'files_startup_view' => 'list',
+    'folder_categories'  => [
+        'file' => [
+            'folder_name'  => 'files',
+            'startup_view' => 'grid',
+            'max_size'     => 50000,
+            'valid_mime'   => [
+                'image/jpeg',
+                'image/pjpeg',
+                'image/png',
+                'image/gif',
+                'image/svg+xml',
+            ]
+        ],
+        'image' => [
+            'folder_name'  => 'photos',
+            'startup_view' => 'list',
+            'max_size'     => 50000,
+            'valid_mime'   => [
+                'image/jpeg',
+                'image/pjpeg',
+                'image/png',
+                'image/gif',
+                'image/svg+xml',
+                'application/pdf',
+                'text/plain',
+            ]
+        ]
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -71,52 +78,24 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    // If true, the uploaded file will be renamed to uniqid() + file extension.
     'rename_file' => false,
 
-    // If rename_file set to false and this set to true, then non-alphanumeric characters in filename will be replaced.
-    'alphanumeric_filename' => true,
+    'alphanumeric_filename'  => false,
 
-    // If true, non-alphanumeric folder name will be rejected.
     'alphanumeric_directory' => false,
 
-    // If true, the uploading file's size will be verified for over than max_image_size/max_file_size.
-    'should_validate_size' => false,
+    'should_validate_size'   => false,
 
-    'max_image_size' => 50000,
-    'max_file_size' => 50000,
-
-    // If true, the uploading file's mime type will be valid in valid_image_mimetypes/valid_file_mimetypes.
-    'should_validate_mime' => false,
-
-    // available since v1.3.0
-    'valid_image_mimetypes' => [
-        'image/jpeg',
-        'image/pjpeg',
-        'image/png',
-        'image/gif',
-        'image/svg+xml',
-    ],
-
-    // available since v1.3.0
-    // only when '/laravel-filemanager?type=Files'
-    'valid_file_mimetypes' => [
-        'image/jpeg',
-        'image/pjpeg',
-        'image/png',
-        'image/gif',
-        'image/svg+xml',
-        'application/pdf',
-        'text/plain',
-    ],
+    'should_validate_mime'   => false,
 
     /*
     |--------------------------------------------------------------------------
-    | Image / Folder Setting
+    | Thumbnail Dimensions
     |--------------------------------------------------------------------------
     */
 
-    'thumb_img_width' => 200,
+    'thumb_img_width'  => 200,
+
     'thumb_img_height' => 200,
 
     /*
@@ -167,7 +146,6 @@ return [
     | directives are not supported.
     */
     'php_ini_overrides' => [
-        'memory_limit'        => '256M'
+        'memory_limit' => '256M'
     ]
-
 ];
