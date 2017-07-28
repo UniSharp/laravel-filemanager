f836ded358d980f2db19951d667d91c745a446b5
ChenCatherine <catherine@unisharp.com>
2017-07-28 19:06:21 +0800

    Change route format for support larval 5.0

--- src/routes.php
+++ src/routes.php
@@ -5,96 +5,96 @@ $middleware = array_merge(\Config::get('lfm.middlewares'), [
     '\Unisharp\Laravelfilemanager\middlewares\CreateDefaultFolder',
 ]);
 $prefix = \Config::get('lfm.prefix', 'laravel-filemanager');
-$as = 'unisharp.lfm.';
+$as = 'unisharp.lfm';
 $namespace = '\Unisharp\Laravelfilemanager\controllers';
 
 // make sure authenticated
-Route::group(compact('middleware', 'prefix', 'as', 'namespace'), function () {
+Route::group(compact('middleware', 'prefix', 'namespace'), function () use ($as) {
 
     // Show LFM
     Route::get('/', [
         'uses' => 'LfmController@show',
-        'as' => 'show',
+        'as' => "{$as}.show",
     ]);
 
     // Show integration error messages
     Route::get('/errors', [
         'uses' => 'LfmController@getErrors',
-        'as' => 'getErrors',
+        'as' => "{$as}.getErrors",
     ]);
 
     // upload
     Route::any('/upload', [
         'uses' => 'UploadController@upload',
-        'as' => 'upload',
+        'as' => "{$as}.upload",
     ]);
 
     // list images & files
     Route::get('/jsonitems', [
         'uses' => 'ItemsController@getItems',
-        'as' => 'getItems',
+        'as' => "{$as}.getItems",
     ]);
 
     // folders
     Route::get('/newfolder', [
         'uses' => 'FolderController@getAddfolder',
-        'as' => 'getAddfolder',
+        'as' => "{$as}.getAddfolder",
     ]);
     Route::get('/deletefolder', [
         'uses' => 'FolderController@getDeletefolder',
-        'as' => 'getDeletefolder',
+        'as' => "{$as}.getDeletefolder",
     ]);
     Route::get('/folders', [
         'uses' => 'FolderController@getFolders',
-        'as' => 'getFolders',
+        'as' => "{$as}.getFolders",
     ]);
 
     // crop
     Route::get('/crop', [
         'uses' => 'CropController@getCrop',
-        'as' => 'getCrop',
+        'as' => "{$as}.getCrop",
     ]);
     Route::get('/cropimage', [
         'uses' => 'CropController@getCropimage',
-        'as' => 'getCropimage',
+        'as' => "{$as}.getCropimage",
     ]);
     Route::get('/cropnewimage', [
         'uses' => 'CropController@getNewCropimage',
-        'as' => 'getCropimage',
+        'as' => "{$as}.getCropimage",
     ]);
 
     // rename
     Route::get('/rename', [
         'uses' => 'RenameController@getRename',
-        'as' => 'getRename',
+        'as' => "{$as}.getRename",
     ]);
 
     // scale/resize
     Route::get('/resize', [
         'uses' => 'ResizeController@getResize',
-        'as' => 'getResize',
+        'as' => "{$as}.getResize",
     ]);
     Route::get('/doresize', [
         'uses' => 'ResizeController@performResize',
-        'as' => 'performResize',
+        'as' => "{$as}.performResize",
     ]);
 
     // download
     Route::get('/download', [
         'uses' => 'DownloadController@getDownload',
-        'as' => 'getDownload',
+        'as' => "{$as}.getDownload",
     ]);
 
     // delete
     Route::get('/delete', [
         'uses' => 'DeleteController@getDelete',
-        'as' => 'getDelete',
+        'as' => "{$as}.getDelete",
     ]);
 
     Route::get('/demo', 'DemoController@index');
 });
 
-Route::group(compact('prefix', 'as', 'namespace'), function () {
+Route::group(compact('prefix', 'namespace'), function () {
     // Get file when base_directory isn't public
     $images_url = '/' . \Config::get('lfm.images_folder_name') . '/{base_path}/{image_name}';
     $files_url = '/' . \Config::get('lfm.files_folder_name') . '/{base_path}/{file_name}';
