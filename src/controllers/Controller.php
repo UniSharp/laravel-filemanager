7e89ed41fd34f783b2a7d74a64514c08f6a09ce0
ChenCatherine <catherine@unisharp.com>
2017-07-28 19:05:27 +0800

    Remove DispatchesJobs for supporting larval 5.0

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -2,11 +2,10 @@
 
 namespace Unisharp\Laravelfilemanager\controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 abstract class Controller extends BaseController
 {
-    use DispatchesJobs, ValidatesRequests;
+    use ValidatesRequests;
 }
