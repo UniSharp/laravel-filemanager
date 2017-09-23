66545579377fedd85ed125fe42e8ca21c863feb8
Youchen Lee <copyleft@utcr.org>
2017-09-24 01:36:25 +0800

    Fix #422 - File/Folder renaming issue.

--- src/controllers/RenameController.php
+++ src/controllers/RenameController.php
@@ -22,47 +22,65 @@ class RenameController extends LfmController
         $new_name = parent::translateFromUtf8(trim(request('new_name')));
 
         $old_file = parent::getCurrentPath($old_name);
+        if (File::isDirectory($old_file)) {
+            return $this->renameDirectory($old_name, $new_name);
+        } else {
+            return $this->renameFile($old_name, $new_name);
+        }
+    }
 
+    protected function renameDirectory($old_name, $new_name)
+    {
         if (empty($new_name)) {
-            if (File::isDirectory($old_file)) {
-                return parent::error('folder-name');
-            } else {
-                return parent::error('file-name');
-            }
+            return parent::error('folder-name');
         }
 
-        if (! File::isDirectory($old_file)) {
-            $extension = File::extension($old_file);
-            $new_name = str_replace('.' . $extension, '', $new_name) . '.' . $extension;
-        }
-
+        $old_file = parent::getCurrentPath($old_name);
         $new_file = parent::getCurrentPath($new_name);
 
-        if (File::isDirectory($old_file)) {
-            event(new FolderIsRenaming($old_file, $new_file));
-        } else {
-            event(new ImageIsRenaming($old_file, $new_file));
-        }
+        event(new FolderIsRenaming($old_file, $new_file));
 
         if (config('lfm.alphanumeric_directory') && preg_match('/[^\w-]/i', $new_name)) {
             return parent::error('folder-alnum');
-        } elseif (File::exists($new_file)) {
+        }
+
+        if (File::exists($new_file)) {
             return parent::error('rename');
         }
 
-        if (File::isDirectory($old_file)) {
-            File::move($old_file, $new_file);
-            event(new FolderWasRenamed($old_file, $new_file));
+        File::move($old_file, $new_file);
+        event(new FolderWasRenamed($old_file, $new_file));
 
-            return parent::$success_response;
+        return parent::$success_response;
+    }
+
+    protected function renameFile($old_name, $new_name)
+    {
+        if (empty($new_name)) {
+            return parent::error('file-name');
         }
 
-        if (parent::fileIsImage($old_file)) {
+        $old_file = parent::getCurrentPath($old_name);
+        $extension = File::extension($old_file);
+        $new_file = parent::getCurrentPath(basename($new_name, ".$extension") . ".$extension");
+
+        if (config('lfm.alphanumeric_filename') && preg_match('/[^\w-.]/i', $new_name)) {
+            return parent::error('file-alnum');
+        }
+
+        // TODO Should be "FileIsRenaming"
+        event(new ImageIsRenaming($old_file, $new_file));
+
+        if (File::exists($new_file)) {
+            return parent::error('rename');
+        }
+
+        if (parent::fileIsImage($old_file) && File::exists(parent::getThumbPath($old_name))) {
             File::move(parent::getThumbPath($old_name), parent::getThumbPath($new_name));
         }
 
         File::move($old_file, $new_file);
-
+        // TODO Should be "FileWasRenamed"
         event(new ImageWasRenamed($old_file, $new_file));
 
         return parent::$success_response;
