cc2fcc0e0e731e37c5b77f6818d1b9a196351f32
Stream <stream@unisharp.com>
2017-09-25 15:48:04 +0800

    refactor ItemsController

--- src/controllers/ItemsController.php
+++ src/controllers/ItemsController.php
@@ -11,43 +11,36 @@ class ItemsController extends LfmController
      */
     public function getItems()
     {
-        $sort_type = request('sort_type');
-
-        if ($sort_type == 'time') {
-            $key_to_sort = 'updated';
-        } elseif ($sort_type == 'alphabetic') {
+        if (request('sort_type') == 'alphabetic') {
             $key_to_sort = 'name';
         } else {
-            $key_to_sort = 'updated';
+            $key_to_sort = 'time';
         }
 
         return [
-            'html' => (string) view($this->getView())->with([
+            'html' => (string) view('laravel-filemanager::items')->with([
                 'items' => array_merge(
                     parent::sortByColumn($this->lfm->folders(), $key_to_sort),
                     parent::sortByColumn($this->lfm->files(), $key_to_sort)
                 ),
+                'display' => $this->getDisplayType(),
             ]),
             'working_dir' => $this->lfm->path('working_dir'),
         ];
     }
 
-    private function getView()
+    private function getDisplayType()
     {
+        $type_key = $this->helper->currentLfmType();
+        $startup_view = config('lfm.' . $type_key . 's_startup_view');
+
         $view_type = 'grid';
-        $show_list = request('show_list');
+        $target_display_type = request('show_list') ?: $startup_view;
 
-        if ($show_list === '1') {
-            $view_type = 'list';
-        } elseif (is_null($show_list)) {
-            $type_key = $this->helper->currentLfmType();
-            $startup_view = config('lfm.' . $type_key . 's_startup_view');
-
-            if (in_array($startup_view, ['list', 'grid'])) {
-                $view_type = $startup_view;
-            }
+        if (in_array($target_display_type, ['list', 'grid'])) {
+            $view_type = $target_display_type;
         }
 
-        return 'laravel-filemanager::' . $view_type . '-view';
+        return $view_type;
     }
 }
