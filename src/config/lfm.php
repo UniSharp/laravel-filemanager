87999f4d1ada5e48bea509784cdd5327353880ca
Stream <stream@unisharp.com>
2018-11-30 16:32:46 +0800

    add some comment to config

--- src/config/lfm.php
+++ src/config/lfm.php
@@ -66,7 +66,7 @@ return [
         'file'  => [
             'folder_name'  => 'files',
             'startup_view' => 'grid',
-            'max_size'     => 50000,
+            'max_size'     => 50000, // size in KB
             'valid_mime'   => [
                 'image/jpeg',
                 'image/pjpeg',
@@ -78,7 +78,7 @@ return [
         'image' => [
             'folder_name'  => 'photos',
             'startup_view' => 'list',
-            'max_size'     => 50000,
+            'max_size'     => 50000, // size in KB
             'valid_mime'   => [
                 'image/jpeg',
                 'image/pjpeg',
