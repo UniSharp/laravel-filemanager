6d115701276b1a35dacd58867efccf5faad6dbc1
Andrew Volobuev <starosta_61@bk.ru>
2019-12-06 13:59:38 +0700

    use illuminate Str class

--- src/LfmItem.php
+++ src/LfmItem.php
@@ -3,6 +3,7 @@
 namespace UniSharp\LaravelFilemanager;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Illuminate\Support\Str;
 
 class LfmItem
 {
@@ -21,7 +22,7 @@ class LfmItem
     public function __get($var_name)
     {
         if (!array_key_exists($var_name, $this->attributes)) {
-            $function_name = camel_case($var_name);
+            $function_name = Str::camel($var_name);
             $this->attributes[$var_name] = $this->$function_name();
         }
 
@@ -66,7 +67,7 @@ class LfmItem
     public function isImage()
     {
         if (!$this->isDirectory()) {
-            return starts_with($this->mimeType(), 'image');
+            return Str::startsWith($this->mimeType(), 'image');
         }
 
         return false;
