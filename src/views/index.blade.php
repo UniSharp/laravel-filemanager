6726d5b7aa6c523ba394c477211ee5b1652ee0fe
Redz Latif <redzwanlatif@gmail.com>
2020-11-03 01:07:39 +0800

    asdas

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -14,9 +14,9 @@
 
   <title>{{ trans('laravel-filemanager::lfm.title-page') }}</title>
   <link rel="shortcut icon" type="image/png" href="{{ asset('vendor/laravel-filemanager/img/72px color.png') }}">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-  <link rel="stylesheet" href="https//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.css">
+  <link rel="stylesheet" href="{{ asset('vendor/laravel-filemanager/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('vendor/laravel-filemanager/css/all.css') }}">
+  <link rel="stylesheet" href="{{ asset('vendor/laravel-filemanager/css/jquery-ui.min.css') }}">
   <link rel="stylesheet" href="{{ asset('vendor/laravel-filemanager/css/cropper.min.css') }}">
   <link rel="stylesheet" href="{{ asset('vendor/laravel-filemanager/css/dropzone.min.css') }}">
   <link rel="stylesheet" href="{{ asset('vendor/laravel-filemanager/css/mime-icons.min.css') }}">
@@ -196,10 +196,10 @@
     </a>
   </div>
 
-  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+  <script src="{{ asset('vendor/laravel-filemanager/js/jquery-3.2.1.min.js') }}"></script>
+  <script src="{{ asset('vendor/laravel-filemanager/js/popper.min.js') }}"></script>
+  <script src="{{ asset('vendor/laravel-filemanager/js/bootstrap.min.js') }}"></script>
+  <script src="{{ asset('vendor/laravel-filemanager/js/jquery-ui.min.js') }}"></script>
   <script src="{{ asset('vendor/laravel-filemanager/js/cropper.min.js') }}"></script>
   <script src="{{ asset('vendor/laravel-filemanager/js/dropzone.min.js') }}"></script>
   <script>
