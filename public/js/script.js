630f9e4f596bd1d31fd04a019b8c8c7da8e45fde
João Gabriel Xavier <joaogabriel.informaticapgua@gmail.com>
2020-11-25 15:39:55 -0300

    removed unused function

--- public/js/script.js
+++ public/js/script.js
@@ -588,10 +588,6 @@ function download(items) {
   });
 }
 
-function open(item) {
-  goTo(item.url);
-}
-
 function preview(items) {
   var carousel = $('#carouselTemplate').clone().attr('id', 'previewCarousel').removeClass('d-none');
   var imageTemplate = carousel.find('.carousel-item').clone().removeClass('active');
