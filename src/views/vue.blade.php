20e1981050df7d06deb0d9f623717096065df992
Stream <streamtw.one@gmail.com>
2025-08-22 01:42:17 +0800

    refactor: manage icons with native web component

--- src/views/vue.blade.php
+++ src/views/vue.blade.php
@@ -64,19 +64,58 @@
     import { createApp, ref, onMounted } from 'vue'
     import CounterComponent from "{{ asset('vendor/laravel-filemanager/js/counter.js') }}"
 
+    function deepGet(obj, path, fallbackValue) {
+      path
+        .replace(/\[([^\[\]]*)\]/g, '.$1.')
+        .split('.')
+        .filter(t => t !== '')
+        .reduce((xs, x) => xs?.[x] ?? fallbackValue ?? undefined, obj)
+    }
+
+    function iconElementFactory (templateId) {
+      return class extends HTMLElement {
+        static observedAttributes = ["size"];
+
+        constructor() {
+          super();
+          let template = document.getElementById(templateId);
+          let templateContent = template.content;
+
+          const shadowRoot = this.attachShadow({ mode: "open" });
+          shadowRoot.appendChild(templateContent.cloneNode(true));
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+          if (name === 'size') {
+            const svg = this.shadowRoot.querySelector('svg')
+            svg.setAttribute("height", newValue)
+            svg.setAttribute("width", newValue)
+          }
+        }
+      }
+    }
+
+    const iconComponents = [
+      'icon-home',
+      'icon-arrow-up',
+      'icon-arrow-down',
+      'icon-grid',
+      'icon-list',
+      'icon-more',
+      'icon-pen',
+      'icon-move',
+      'icon-trash',
+    ]
+    iconComponents.map(iconName => {
+      customElements.define(iconName, iconElementFactory(iconName))
+    })
+
     createApp({
       components: {
         CounterComponent
       },
       template: '#main-template',
       setup() {
-        function deepGet(obj, path, fallbackValue) {
-          path
-            .replace(/\[([^\[\]]*)\]/g, '.$1.')
-            .split('.')
-            .filter(t => t !== '')
-            .reduce((xs, x) => xs?.[x] ?? fallbackValue ?? undefined, obj)
-        }
 
         const message = ref('Hello vue!')
 
@@ -114,6 +153,173 @@
       }
     }).mount('#vue-app')
   </script>
+
+  <template id="icon-home">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M21 8.77217L14.0208 1.79299C12.8492 0.621414 10.9497 0.621413 9.77817 1.79299L3 8.57116V23.0858H10V17.0858C10 15.9812 10.8954 15.0858 12 15.0858C13.1046 15.0858 14 15.9812 14 17.0858V23.0858H21V8.77217ZM11.1924 3.2072L5 9.39959V21.0858H8V17.0858C8 14.8767 9.79086 13.0858 12 13.0858C14.2091 13.0858 16 14.8767 16 17.0858V21.0858H19V9.6006L12.6066 3.2072C12.2161 2.81668 11.5829 2.81668 11.1924 3.2072Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
+
+  <template id="icon-arrow-up">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.6568 8.96219L16.2393 10.3731L12.9843 7.10285L12.9706 20.7079L10.9706 20.7059L10.9843 7.13806L7.75404 10.3532L6.34314 8.93572L12.0132 3.29211L17.6568 8.96219Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
+
+  <template id="icon-arrow-down">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.0001 3.67157L13.0001 3.67157L13.0001 16.4999L16.2426 13.2574L17.6568 14.6716L12 20.3284L6.34314 14.6716L7.75735 13.2574L11.0001 16.5001L11.0001 3.67157Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
+
+  <template id="icon-grid">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M11 7H7V11H11V7Z" fill="currentColor" />
+      <path d="M11 13H7V17H11V13Z" fill="currentColor" />
+      <path d="M13 13H17V17H13V13Z" fill="currentColor" />
+      <path d="M17 7H13V11H17V7Z" fill="currentColor" />
+    </svg>
+  </template>
+
+  <template id="icon-list">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M9 7H7V9H9V7Z" fill="currentColor" />
+      <path d="M7 13V11H9V13H7Z" fill="currentColor" />
+      <path d="M7 15V17H9V15H7Z" fill="currentColor" />
+      <path d="M11 15V17H17V15H11Z" fill="currentColor" />
+      <path d="M17 13V11H11V13H17Z" fill="currentColor" />
+      <path d="M17 7V9H11V7H17Z" fill="currentColor" />
+    </svg>
+  </template>
+
+  <template id="icon-more">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M5 15C6.65685 15 8 13.6569 8 12C8 10.3431 6.65685 9 5 9C3.34315 9 2 10.3431 2 12C2 13.6569 3.34315 15 5 15ZM5 13C5.55228 13 6 12.5523 6 12C6 11.4477 5.55228 11 5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15ZM12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M22 12C22 13.6569 20.6569 15 19 15C17.3431 15 16 13.6569 16 12C16 10.3431 17.3431 9 19 9C20.6569 9 22 10.3431 22 12ZM20 12C20 12.5523 19.5523 13 19 13C18.4477 13 18 12.5523 18 12C18 11.4477 18.4477 11 19 11C19.5523 11 20 11.4477 20 12Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
+
+  <template id="icon-pen">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M21.2635 2.29289C20.873 1.90237 20.2398 1.90237 19.8493 2.29289L18.9769 3.16525C17.8618 2.63254 16.4857 2.82801 15.5621 3.75165L4.95549 14.3582L10.6123 20.0151L21.2189 9.4085C22.1426 8.48486 22.338 7.1088 21.8053 5.99367L22.6777 5.12132C23.0682 4.7308 23.0682 4.09763 22.6777 3.70711L21.2635 2.29289ZM16.9955 10.8035L10.6123 17.1867L7.78392 14.3582L14.1671 7.9751L16.9955 10.8035ZM18.8138 8.98525L19.8047 7.99429C20.1953 7.60376 20.1953 6.9706 19.8047 6.58007L18.3905 5.16586C18 4.77534 17.3668 4.77534 16.9763 5.16586L15.9853 6.15683L18.8138 8.98525Z"
+        fill="currentColor"
+      />
+      <path
+        d="M2 22.9502L4.12171 15.1717L9.77817 20.8289L2 22.9502Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
+
+  <template id="icon-move">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13 5V3L21 3V11H19V6.41424L13.6361 11.7782C13.2456 12.1687 12.6124 12.1687 12.2219 11.7782C11.8314 11.3876 11.8314 10.7545 12.2219 10.3639L17.5858 5L13 5Z"
+        fill="currentColor"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M5 13C3.89543 13 3 13.8954 3 15L3 19C3 20.1046 3.89543 21 5 21H9C10.1046 21 11 20.1046 11 19V15C11 13.8954 10.1046 13 9 13H5ZM5 15L5 19H9V15H5Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
+
+  <template id="icon-trash">
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M17 6V5C17 3.89543 16.1046 3 15 3H9C7.89543 3 7 3.89543 7 5V6H4C3.44772 6 3 6.44772 3 7C3 7.55228 3.44772 8 4 8H5V19C5 20.6569 6.34315 22 8 22H16C17.6569 22 19 20.6569 19 19V8H20C20.5523 8 21 7.55228 21 7C21 6.44772 20.5523 6 20 6H17ZM15 5H9V6H15V5ZM17 8H7V19C7 19.5523 7.44772 20 8 20H16C16.5523 20 17 19.5523 17 19V8Z"
+        fill="currentColor"
+      />
+    </svg>
+  </template>
 
   <!-- <h1 x-data="{ message: 'I ❤️ Alpine' }" x-text="message"></h1>
   <div x-data="{ count: 0 }">
